05472ef993bf2471147d59ed7e91e6e894005375
marcantoineandreoli@gmail.com Andreoli <marcantoineandreoli@gmail.com>
2018-03-21 14:09:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var recipes = {};
 
-function updateObjectWithKeyAndValue(recipes, key, value) {
-  recipes[key] = value;
-  return recipes;
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 };
+
+function destructivelyUpdateObjectWithKeyAndValue(object,
+ key, value) {
+   object[key] = value;
+   return object;
+ };
